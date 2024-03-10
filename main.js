19c4bc55ceac2c09b269aea2442e35737d2a04e9
kareemelbalshe <89804459+kareemelbalshe@users.noreply.github.com>
2024-03-11 01:25:57 +0200

    init

--- main.js
+++ main.js
@@ -1,48 +1,30 @@
-const itemHeaders = document.querySelectorAll(".accordion-item-header");
-const i1=document.getElementById("i1")
-const i2=document.getElementById("i2")
-const i3=document.getElementById("i3")
-const i4=document.getElementById("i4")
-const i5=document.getElementById("i5")
-const i6=document.getElementById("i6")
+const menu = document.querySelector('.nav_menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
 
-itemHeaders.forEach((accordion) => {
-  accordion.addEventListener("click", collapseAccordions);
 
-  function collapseAccordions() {
-    const activeAccordion = document.querySelector(".active");
-    if (activeAccordion && activeAccordion !== accordion) {
-      activeAccordion.classList.toggle("active");
-      activeAccordion.nextElementSibling.style.maxHeight = 0;
+window.addEventListener('load', () => {
+    if (window.innerWidth > 1024) {
+        menuBtn.style.display = "none"
+        closeBtn.style.display = "none"
+        menu.style.display = "flex"
     }
-
-    accordion.classList.toggle("active");
-    const accordionItemBody = accordion.nextElementSibling;
-
-    if (accordion.classList.contains("active")) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    } else {
-      accordionItemBody.style.maxHeight = 0;
+    else {
+        menu.style.display = 'none';
+        closeBtn.style.display = 'none';
+        menuBtn.style.display = 'inline-block';
     }
-  }
-});
-window.addEventListener("load",()=>{
-    setInterval(()=>{
-        i1.style.opacity="1"
-    },1500)
-    setInterval(()=>{
-        i2.style.opacity="1"
-    },2000)
-    setInterval(()=>{
-        i3.style.opacity="1"
-    },2500)
-    setInterval(()=>{
-        i4.style.opacity="1"
-    },3000)
-    setInterval(()=>{
-        i5.style.opacity="1"
-    },3500)
-    setInterval(()=>{
-        i6.style.opacity="1"
-    },4000)
 })
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    menuBtn.style.display = 'none';
+})
+
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+}
+
+closeBtn.addEventListener('click', closeNav);
