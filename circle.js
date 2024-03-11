92d2f4e21f1865d65f777a905df140cd410e1c3e
kareemelbalshe <89804459+kareemelbalshe@users.noreply.github.com>
2024-03-11 02:11:40 +0200

    aaaa

--- circle.js
+++ circle.js
@@ -1,4 +1,3 @@
-const itemHeaders = document.querySelectorAll(".accordion-item-header");
 const i1=document.getElementById("i1")
 const i2=document.getElementById("i2")
 const i3=document.getElementById("i3")
@@ -6,26 +5,7 @@ const i4=document.getElementById("i4")
 const i5=document.getElementById("i5")
 const i6=document.getElementById("i6")
 
-itemHeaders.forEach((accordion) => {
-  accordion.addEventListener("click", collapseAccordions);
 
-  function collapseAccordions() {
-    const activeAccordion = document.querySelector(".active");
-    if (activeAccordion && activeAccordion !== accordion) {
-      activeAccordion.classList.toggle("active");
-      activeAccordion.nextElementSibling.style.maxHeight = 0;
-    }
-
-    accordion.classList.toggle("active");
-    const accordionItemBody = accordion.nextElementSibling;
-
-    if (accordion.classList.contains("active")) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    } else {
-      accordionItemBody.style.maxHeight = 0;
-    }
-  }
-});
 window.addEventListener("load",()=>{
     setInterval(()=>{
         i1.style.opacity="1"
