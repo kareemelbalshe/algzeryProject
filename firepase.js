330c9a2ddbb6387b4e7e9b44d32f1ce3d50f9ae5
Ahmed Algzery <algzeryahmed14@gmail.com>
2024-03-14 14:36:52 +0200

    .

--- firepase.js
+++ firepase.js
@@ -7,22 +7,16 @@ import { collection, doc, getDoc, getFirestore } from "https://www.gstatic.com/f
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const docRef = doc(db, "article", "zaOskqpNOBc7GmReb67f");
+const docRef = doc(db, "article", "CYQvwXQqPc5gP0P7MGEi");
 
 // Function to get document data
 async function getDocumentData() {
+
 
   try {
     const docSnap = await getDoc(docRef);
@@ -56,6 +50,7 @@ async function getDocumentData() {
 
 // Call the function to get document data
 getDocumentData();
+
 
 //      var docRef = db.collection("article").doc("zaOskqpNOBc7GmReb67f");
 
