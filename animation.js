b5763478730df49fe0dc8fe491f159c642729c75
kareemelbalshe <89804459+kareemelbalshe@users.noreply.github.com>
2024-03-14 07:04:16 +0200

    ya my head

--- animation.js
+++ animation.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const sections = document.querySelectorAll('section');
+    const sections = document.querySelectorAll('article');
 
     window.addEventListener('scroll', function() {
         sections.forEach(function(section) {
@@ -17,27 +17,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const sections = document.querySelectorAll('article');
-
-    window.addEventListener('scroll', function() {
-        sections.forEach(function(section,i) {
-            const sectionTop = section.getBoundingClientRect().top;
-            const windowHeight = window.innerHeight;
-
-            if (sectionTop < windowHeight * 0.75) {
-                section.style.opacity = 1;
-                section.style.transform= 'translateX(0)';
-            } else {
-                if(i%2===0){
-                    section.style.opacity = 0;
-                    section.style.transform= 'translateX(-100%)';
-                }
-                else{
-                    section.style.opacity = 0;
-                    section.style.transform= 'translateX(100%)';
-                }
-            }
-        });
-    });
-});
--- firepase.js
+++ firepase.js
@@ -1,31 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
-import { collection, doc, getDoc, getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { doc, getDoc, getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
-};
-
+import { firebaseConfig } from "./firebaseConfig.js";
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const docRef = doc(db, "article", "zaOskqpNOBc7GmReb67f");
-
+const articleRef = doc(db, "article", "zaOskqpNOBc7GmReb67f");
 // Function to get document data
-async function getDocumentData() {
+async function getArticleData() {
 
   try {
-    const docSnap = await getDoc(docRef);
+    const docSnap = await getDoc(articleRef);
     if (docSnap.exists()) {
       const data = docSnap.data()
       const article = document.getElementById("a")
@@ -55,17 +45,49 @@ async function getDocumentData() {
 }
 
 // Call the function to get document data
-getDocumentData();
+getArticleData();
 
-//      var docRef = db.collection("article").doc("zaOskqpNOBc7GmReb67f");
 
-// docRef.get().then((doc) => {
-//     if (doc.exists) {
-//         console.log("Document data:", doc.data());
-//     } else {
-//         // doc.data() will be undefined in this case
-//         console.log("No such document!");
-//     }
-// }).catch((error) => {
-//     console.log("Error getting document:", error);
-// });
+
+const portfolioRef = doc(db, "portfolio", "7V7gYNlWdHNN2r7jrBO5");
+// Function to get document data
+async function getPortfolioData() {
+
+  try {
+    const docSnap = await getDoc(portfolioRef);
+    if (docSnap.exists()) {
+      const data = docSnap.data()
+      const article = document.getElementById("p")
+      var result = Object.keys(data).map((key) => [key, data[key]]);
+      console.log(result)
+      result.map(element => {
+        console.log(element)
+        article.innerHTML += `
+        <div class="card">
+        <img src="${element[1].img}" alt="" />
+        <div class="info-card">
+          <h3>${element[1].title}</h3>
+          <p>
+          ${element[1].dis}
+          </p>
+        </div>
+      </div>
+    `
+      });
+    } else {
+      console.log("No such document!");
+    }
+  } catch (error) {
+    console.error("Error getting document:", error);
+  }
+}
+
+// Call the function to get document data
+getPortfolioData();
+
+
+
+
+
+
+
