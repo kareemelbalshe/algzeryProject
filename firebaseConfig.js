18d18f50865e8f44b39f363032a768af2f6d0a72
kareemelbalshe <89804459+kareemelbalshe@users.noreply.github.com>
2024-07-02 02:27:31 +0300

    new

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDPMB2vr4HlW7iHiLURstZuJg4NkdGqNk4",
+    authDomain: "firest-47b56.firebaseapp.com",
+    projectId: "firest-47b56",
+    storageBucket: "firest-47b56.appspot.com",
+    messagingSenderId: "27708142474",
+    appId: "1:27708142474:web:b4615041683f2e4a454e73",
+    measurementId: "G-287PBF6HRR"
+  };
